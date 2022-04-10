afae5b364076014be86f7c60be95ad0598e6e2c8
Sergio <sergiosilva@outlook.com>
2022-04-10 18:39:16 -0300

    user input comparasion with the letter

--- app.rb
+++ app.rb
@@ -4,30 +4,42 @@ class Hangman
   attr_accessor :words, :available_words, :name, :random
 
   def initialize
-    @words = File.read('words.txt').split
+    @words = ['banana'] # File.read('words.txt').split
     @letters = ('a'..'z').to_a
     @available_words = []
-    @random = ''
-    random_word
+    @random = []
+    p random_word
+    p @lives = @random.length + 2
     start
-    
+    @word_length = []
   end
 
   def start
-    @player_input = []
-    puts "Hi! Your word have #{@random.length} letters"
-    puts "\n"
+    @player_guess = []
+    @word_length = ''
+    puts "Your word have #{@random.length} letters."
+    puts ''
+    @random.length.times do
+      @word_length = @word_length += '_ '
+    end
+    puts @word_length.to_s
     puts 'Enter a letter:'
+    player_input
+  end
+
+  def player_input
     i = false
     until i == true
       input = gets.chomp.downcase
       if /[a-z]{1}/.match?(input) && input.length == 1
         i = true
-        @player_input = input
-        return @player_input
+        @player_guess = input
+        puts "Your guess is #{@player_guess}"
+        p @player_guess.split('')
       else
         puts 'Please, input a letter'
       end
+      guess
     end
   end
 
@@ -39,9 +51,19 @@ class Hangman
     @random = @available_words.sample
   end
 
-  def player_guess; end
-
-  def def(render_display); end
+  def guess
+    @hint = []
+    if p @random.include?(@player_guess)
+      @random.split('').each do |i|
+        if @player_guess[i] == @random[i]
+          @hint[i] = @player_guess[i]
+          p @hint
+      end
+    else
+      @lives -= 1
+      puts "Wrong! You have #{@lives} left"
+    end
+  end
 end
 
 game = Hangman.new
