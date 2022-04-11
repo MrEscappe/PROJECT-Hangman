cfd3441ebfff720a289a40f907fc6db108d66482
Sergio <sergiosilva@outlook.com>
2022-04-11 18:54:08 -0300

    working on the display feedback

--- app.rb
+++ app.rb
@@ -5,42 +5,67 @@ class Hangman
 
   def initialize
     @words = ['banana'] # File.read('words.txt').split
-    @letters = ('a'..'z').to_a
     @available_words = []
     @random = []
-    p random_word
-    p @lives = @random.length + 2
+    @correct_guess = []
+    random_word
+    @lives = @random.length + 2
     start
-    @word_length = []
+    @word_length = ''
   end
 
   def start
-    @player_guess = []
     @word_length = ''
     puts "Your word have #{@random.length} letters."
     puts ''
-    @random.length.times do
-      @word_length = @word_length += '_ '
-    end
-    puts @word_length.to_s
-    puts 'Enter a letter:'
+    render
     player_input
+  end
+
+  def render
+    @random.length.times do
+      @board = @word_length += '_ '
+      @board
+    end
+    puts @board.to_s
+  end
+
+  def render_game(last_guess)
+    i = 0
+    while i < @random.size
+      if p last_guess == @random[i]
+        @board[i] = @random[i]
+      end
+      i += 1
+    end
+  end
+
+  def update_render
+    puts @board
   end
 
   def player_input
     i = false
+    puts ''
+    puts 'If you want to save just write (save) and if you want to load the game just write (load).'
+    puts 'To play just input a letter:'
+    input = gets.chomp.downcase
     until i == true
-      input = gets.chomp.downcase
+      case input
+      when 'save'
+        save_method
+      when 'load'
+        load_method
+      end
       if /[a-z]{1}/.match?(input) && input.length == 1
         i = true
         @player_guess = input
-        puts "Your guess is #{@player_guess}"
-        p @player_guess.split('')
       else
         puts 'Please, input a letter'
+        player_input
       end
-      guess
     end
+    guess
   end
 
   def random_word
@@ -52,16 +77,23 @@ class Hangman
   end
 
   def guess
-    @hint = []
-    if p @random.include?(@player_guess)
-      @random.split('').each do |i|
-        if @player_guess[i] == @random[i]
-          @hint[i] = @player_guess[i]
-          p @hint
+    if @lives > 1
+      if @random.include?(@player_guess)
+        @correct_guess << @player_guess
+        puts 'Right letter!'
+      else
+        @lives -= 1
+        puts "Wrond letter! You have #{@lives} lives left."
+
       end
+
+      render_game(@player_guess)
+      update_render
+      player_input
     else
-      @lives -= 1
-      puts "Wrong! You have #{@lives} left"
+      puts 'Too bad! You are out of lives!'
+      puts ''
+      puts 'GAME OVER!'
     end
   end
 end
